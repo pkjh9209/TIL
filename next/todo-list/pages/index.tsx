3bdb7be2a429e48829f46b20d0b9c883ad796239
pkjh9209 <pkjh20177@gmail.com>
2022-12-14 14:44:58 +0900

    화면 수정 - 책 에러 , svg 웹팩 추가, svg 아이콘 적용

--- next/todo-list/pages/index.tsx
+++ next/todo-list/pages/index.tsx
@@ -9,7 +9,7 @@ import {getTodoAPI} from "../lib/api/todo"
 
 
 const todos : TodoType[] = [
-  {id :1 , text: "마트가서 장보기111",color: "red", checked : false},
+  {id :1 , text: "편의점",color: "red", checked : false},
   {id :2 , text: "숙제하기",color: "orange", checked : false},
   {id :3 , text: "코딩하기",color: "yellow", checked : true},
   {id :4 , text: "넥스트공부하기",color: "green", checked : false},
@@ -17,13 +17,19 @@ const todos : TodoType[] = [
   {id :6 , text: "퇴근하기",color: "navy", checked : false},
 ];
 
-const app : NextPage = () => {
+
+interface IProps{
+  todos: TodoType[];
+}
+
+const app : NextPage<IProps> = () => {
+  // console.log(process.env,"클라이언트");
   return <TodoList todos = {todos} />;
 }
 
-
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
+    // console.log(process.env,"서버");
     const {data} = await getTodoAPI();
     console.log(data);
     return {props:{}};
